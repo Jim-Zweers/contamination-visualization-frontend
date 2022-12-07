0ce2e44d9f0bd945e87c4efd0420621621e4a953
Felipe Noordegraaf <fnoordegraaf@gmail.com>
2022-12-07 14:30:43 +0100

    3d model can be loaded into vue

--- src/components/ThreeCanvas.vue
+++ src/components/ThreeCanvas.vue
@@ -4,39 +4,30 @@
 
 <script>
     import * as THREE from 'three';
-    import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+    import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
     // import OrbitControls from 'three/examples/jsm/controls/OrbitControls';
     
     export default {
         setup(){
-            
+            const MODEL = "./models/Kwast.glb";
+
             const render = () => {
                 const renderer = new THREE.WebGLRenderer();
                 renderer.setSize( window.innerWidth, window.innerHeight);
                 document.body.appendChild( renderer.domElement );
 
                 const scene = new THREE.Scene();
+                const light = new THREE.AmbientLight(0x404040);
                 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
                 const loader = new GLTFLoader();
-                console.log("hallo");
-                console.log(loader);
                 let root;
-                loader.load(
-                    'Kwast.glb', (gltf) =>{
-                    console.log(gltf);
-                    root = gltf.scene;
-                    console.log(root);
-                    scene.add(root);
-                }, function (xhr){
-                    console.log((xhr.loaded/xhr.total * 100)+ "% loaded")
-                }
-
-                );
-
+                console.log(loader);
+                loader.load(MODEL, (gltf) =>{root = gltf.scene; scene.add(root)});
                 function animate(){
                 requestAnimationFrame( animate );
                 renderer.render( scene, camera );
                 }
+                scene.add(light);
                 animate();
                 camera.position.z = 5;
             }
